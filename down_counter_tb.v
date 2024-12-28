7307d9078bc151721a7d66f39e7a4f263f9b9703
greentextterminal <68518334+greentextterminal@users.noreply.github.com>
2024-12-27 18:57:55 -0600

    stuff

--- down_counter_tb.v
+++ down_counter_tb.v
@@ -1,13 +1,13 @@
+`timescale 1ns / 1ps
 // testbench for down_counter
 
 module down_counter_tb;
-    // inputs are registers
-    // outputs are wires since they're continuously driven
-    reg  clock, reset;
-    wire count;
-
     // parameters for user to set
-    parameter N = 3;
+    parameter N = 3;    
+    // inputs are registers
+    reg  clock, reset;
+    // outputs are wires since they're continuously driven
+    wire [N:0] count;
 
     // instantiating the down_counter
     down_counter #(.n(N)) uut 
@@ -29,7 +29,7 @@ module down_counter_tb;
     // creating the oscillating clock
     always #10 clock = ~clock;
 
-    // trigger reset to start counter at max value then make reset low
+    // set reset high to start counter at max value then set reset low
     initial 
     begin
         reset = 1'b1;
